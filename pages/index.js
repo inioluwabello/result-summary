53038c929b4fd5e8b987c092ddaf24426549cab0
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-08 16:55:23 +0100

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
 import Layout from "../components/layout";
 import Heading from "../components/heading";
-import FormComponent from "../components/FormComponent";
-import { useState } from "react";
-import DisplayComponent from "../components/DisplayComponent";
 
 const Index = () => {
-  const [age, setAge] = useState({years: "- -", months: "- -", days: "- -"});
 
   return (
     <>
       <Heading title={"Age Calculator"} />
       <Layout>
-        <div className="centered-content">
-          <div className="content-wrapper white">
-            <FormComponent setAge={setAge} />
-            <DisplayComponent age={age} />
+        <div className="centered-content HankenGrotesk-Regular">
+          <div className="content-wrapper">
+           
           </div>
         </div>
       </Layout>
